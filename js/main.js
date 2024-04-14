9ac79e2c93eb2c479e9bd7d58e184ffee7c298f2
benjamin <benjamin.marissiaux@student.hepl.be>
2024-04-14 16:16:59 +0200

    apparition-au-scroll

--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
 import {settings} from "./settings";
 
+//apparation de la nav principale au scroll
 settings.noJs.classList.add('js_enabled');
 settings.header.classList.remove('no_js');
 settings.monLogoChange.src = settings.whiteLogoSource;
@@ -14,7 +15,7 @@ window.addEventListener("scroll", e => {
         settings.header.classList.add(settings.navAppeared);
     }
 
-    if (window.scrollY === 0){
+    if (window.scrollY === 0) {
         settings.header.classList.remove(settings.navAppeared);
         settings.header.classList.remove(settings.navAppear);
         settings.monLogoChange.src = settings.whiteLogoSource;
@@ -27,4 +28,30 @@ window.addEventListener("scroll", e => {
     }
 
 });
+
+//apparation au défilement
+
+const ratio = 0.1;
+const options = {
+    root: null,
+    rootMargin: '0px',
+    threshold: ratio
+}
+
+function handleIntersect(entries, observer) {
+    entries.forEach(entry => {
+        if (entry.intersectionRatio > ratio) {
+            entry.target.classList.add('reveal-visible');
+            observer.unobserve(entry.target);
+        } else {
+        }
+    });
+}
+
+const observer = new IntersectionObserver(handleIntersect, options);
+
+document.querySelectorAll('[class*="reveal-"]').forEach(revealElement => {
+    observer.observe(revealElement);
+});
+
 
